78a8f318ada76959520f4c6b97e9e34d6d15bfbb
Synne Opheim <synnevo@gmail.com>
2015-02-27 13:11:57 +0100

    gjort endringer i klassen

--- src/Appointment/Notification.java
+++ src/Appointment/Notification.java
@@ -1,5 +1,20 @@
 package Appointment;
 
+import user.User;
+
 public class Notification {
 
+	public String message;
+	public User sender;
+	public User reciever;
+	
+	
+	public Notification(String newMessage, User reciever) {
+		this.message = newMessage;
+		this.reciever = reciever;
+	}
+	
+	public void notify() {
+		reciever.inbox.add(this); //ikke opprettet denne klassen enda
+	}
 }
