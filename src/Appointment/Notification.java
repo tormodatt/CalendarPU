28abb4df0d4812d44c01b85d5307603395a2e2eb
Synne Opheim <synnevo@gmail.com>
2015-02-27 13:26:32 +0100

    Opprettet NotificationInbox under user-pakken, og gjort endringer i Notification-klassen

--- src/Appointment/Notification.java
+++ src/Appointment/Notification.java
@@ -1,20 +1,22 @@
 package Appointment;
 
 import user.User;
+import user.NotificationInbox;
 
 public class Notification {
 
 	public String message;
+	public String subject;
 	public User sender;
 	public User reciever;
 	
 	
-	public Notification(String newMessage, User reciever) {
+	public Notification(String subject, String newMessage, User reciever) {
 		this.message = newMessage;
 		this.reciever = reciever;
 	}
 	
 	public void notify() {
-		reciever.inbox.add(this); //ikke opprettet denne klassen enda
+		reciever.notificationInbox.addNotification(this);
 	}
 }
