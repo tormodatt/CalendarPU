8b78876fa136ef3336a4b7f2fbe5d281f2b5d217
sbuadu <steffyquayson@hotmail.com>
2015-03-01 16:44:42 +0100

    startet å lage en test

---
+++ src/user/UserTest.java
@@ -0,0 +1,21 @@
+package user;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class UserTest {
+
+	@Test
+	public boolean test() {
+		User testuser = new User(null, null, null, null, null);
+		String navn = "petter"; 
+		assertTrue(navn.equals(testuser.setNavn(navn))); 
+		
+
+	
+	}
+	
+}
+
+
