bab4aaa6aa16b815733df2abecf7535c842b0145
sbuadu <steffyquayson@hotmail.com>
2015-03-03 08:58:33 +0100

    lagrer endringer

--- src/tests/UserTest.java
+++ src/tests/UserTest.java
@@ -8,16 +8,29 @@ import calendar.PersonalCalendar;
 
 public class UserTest {
 
+	private void testException(Exception e, Class<? extends Exception> c) {
+		assertTrue(e + " should be assignable to " + c, c.isAssignableFrom(e.getClass()));
+
+	}
+
 	@Test //et helt lovlig brukernavn
-	public void testSetUser1() throws Exception {
-		User testbruker = new User("olnord"); 
+	public void testSetUser1(){
+		try {
+			User testbruker = new User("olnord");
+			assertEquals("olnord", testbruker.getUsername()); 
+		} catch (Exception e) {
+			fail("olnord is a valid username"); 
+		} 
 
 	}
 	@Test //det skal ikke være lov med tall i brukernavnet
 	public void testSetUser2(){
 		try{
-			User testbruker = new User("bh1nn");	
+			String name = "bh1nn"; 
+			User testbruker = new User(name);	
+			fail(name + "is an invalid username"); 
 		}catch (Exception e){
+			testException(e, IllegalArgumentException.class);
 		}
 	}
 	@Test //spesielle symboler er ikke lov i et brukernavn
@@ -34,6 +47,7 @@ public class UserTest {
 	public void testSetUser4(){
 		try {
 			User testbruker = new User("ab  vd");
+
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -63,7 +77,7 @@ public class UserTest {
 			testbruker = new User("olnord");
 			testbruker.setCredencials("Ola", "Nordmann", "olnord", "password", "mail@mail.com");
 		}catch (Exception e){
-			
+
 		}
 	}
 
