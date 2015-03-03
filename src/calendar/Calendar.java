07fd7c49129f2f3360e825227655c636569da398
tormodatt <moddad@gmail.com>
2015-03-03 11:57:25 +0100

    konstruktører i Calendar

--- src/calendar/Calendar.java
+++ src/calendar/Calendar.java
@@ -10,14 +10,12 @@ import Appointment.*;
 
 public class Calendar extends Database {
 
-	public ArrayList<Appointment> appointment;
+	public ArrayList<Appointment> appointments;
 	private User user;
 	private Group group;
-	
 	private int calendarID;
 	private String title;
-	private String username;
-	private int groupID;
+	
 	private PreparedStatement preparedStatement = null;
 	private ResultSet resultSet = null;
 	
@@ -53,33 +51,47 @@ public class Calendar extends Database {
 	public Calendar(User user) throws Exception {
 		try {
 			openConn();
-			preparedStatement = connect.prepareStatement("select * from Calendar WHERE Username=?");
+			preparedStatement = connect.prepareStatement("select * from Calendar WHERE User_Username=?");
 			preparedStatement.setString(1,user.getUsername());
 			resultSet = preparedStatement.executeQuery();
 			while (resultSet.next()) {
 					this.calendarID = resultSet.getInt("CalendarID");
 					this.title = resultSet.getString("Title");
 				}
+			setAppointments();
 		} finally {
 				closeConn();
 		}
-		this.username = user.getUsername();
+		this.user = user;
 	}
 
 	//Hente gruppekalender
 	public Calendar(Group group) throws Exception {
 		try {
 			openConn();
-			preparedStatement = connect.prepareStatement("insert into Calendar (Title,User_Username) values (?,?)");
-			preparedStatement.setString(1,title);
-			preparedStatement.setString(2,user.getUsername());
-			preparedStatement.executeUpdate();
-			} finally {
-			closeConn();
+			preparedStatement = connect.prepareStatement("select * from Calendar WHERE Group_GroupID=?");
+			preparedStatement.setString(1,group.getGroupID());
+			resultSet = preparedStatement.executeQuery();
+			while (resultSet.next()) {
+					this.calendarID = resultSet.getInt("CalendarID");
+					this.title = resultSet.getString("Title");
 			}
+			setAppointments();
+		} finally {
+				closeConn();
+		}
 		this.group = group; 
 	}
 	
+	public void setAppointments() {
+		preparedStatement = connect.prepareStatement("select AppointmentID from Appointment WHERE CalendarID = ?");
+		preparedStatement.setInt(1, calendarID);
+		resultSet = preparedStatement.executeQuery();
+		while (resultSet.next()) {
+				//Mangler skikkelig konstrukt¿r i Appointmentklassen
+				this.appointments.add(new Appointment(resultSet.getInt("AppointmentID")));
+		}
+	}
 	
 
 }
