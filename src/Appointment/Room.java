7e03dd9af081b24e4f714c54a8926dcbb3508375
Synne Opheim <synnevo@gmail.com>
2015-03-05 11:02:27 +0100

    a

--- src/Appointment/Room.java
+++ src/Appointment/Room.java
@@ -4,40 +4,42 @@ import java.util.ArrayList;
 
 public class Room {
 	
-	public String roomID; 
+	public String roomName; // skal ikke kunne endres
 	public int capacity;
-	public Appointment appointment; 
 	public ArrayList<Appointment> appointments;
 	
-	public Room(String roomID, int antallPlasser) {
-		this.roomID = roomID;
-		this.capacity = capacity; 
-		this.appointments = new ArrayList<Appointment>(); 
+	
+	public Room(String roomName, int antallPlasser) {
+		this.roomName = roomName;
+		this.capacity = capacity; // antall plasser 
+		this.appointments = new ArrayList<Appointment>(); // må bruke SQL til å finne alle avtalene som er relatert til rommet, og de legges inn i denne
 	}
 
 	public ArrayList<Appointment> getAppointment() {
 		return appointments;
 	}
 	
-	public String getRoomID() {
-		return roomID;
+	public String getRoomName() {
+		return roomName;
 	}
 
 	public int getCapacity() {
 		return capacity;
 	}
 
-	public void setRoomID(String roomID) {
-		this.roomID = roomID; 
-	}
 	
 	public void setCapacity(int capacity) {
-		this.capacity = capacity; 
+		if (capacity > -1) {
+			this.capacity = capacity;
+		} 
+		else {
+			throw new IllegalArgumentException("Capacity must be a positive integer.");
+		}
 	}
 	
 	public void addAppointment(Appointment appointment) {
 		if (appointments.contains(appointment)) {
-			throw new IllegalArgumentException("Avtalen skal allerede finnes sted i rommet"); 
+			throw new IllegalArgumentException("This room is already related to this appointment"); 
 		} else {
 			appointments.add(appointment); 
 		}
@@ -47,7 +49,7 @@ public class Room {
 		if (appointments.contains(appointment)) {
 			 appointments.remove(appointment);
 		} else {
-			throw new IllegalArgumentException("Avtalen er ikke planlagt i dette rommet");
+			throw new IllegalArgumentException("Room and appointment is not related");
 		}
 	}
 	
--- src/Appointment/RoomOverview.java
+++ src/Appointment/RoomOverview.java
@@ -5,31 +5,21 @@ import java.util.ArrayList;
 public class RoomOverview {
 	
 	public ArrayList<Room> allRooms; 
-	public Room room; 
+	
 	
 	public RoomOverview() {
 		this.allRooms = new ArrayList<Room>(); 		
 	}
 	
+	
 	public ArrayList<Room> getRoom(){
 		return this.allRooms; 
 	}
-
-	public void addRoom(Room room) {
-		if (allRooms.contains(room)) {
-			throw new IllegalArgumentException("Rom er allerede lagt til i oversikten"); 
-		} else {
-			allRooms.add(room); 
-		}
-	}
 	
-	public void removeRom(Room room) {
-		if (allRooms.contains(room)) {
-			allRooms.remove(room); 
-		} else {
-			throw new IllegalArgumentException("Rom finnes ikke i oversikten");
-		}
+	public ArrayList<Room> getFreeRooms(Time time) {
+		ArrayList<Room> freeRooms = new ArrayList<Room>();
+		// Må skrive SQL som henter ut alle rom som er ledige i den aktuelle tidsperioden 
+		return freeRooms; //returnerer liste med ledige rom 
 	}
-	
 
 }
