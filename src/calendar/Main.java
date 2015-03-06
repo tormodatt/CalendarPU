252ce257b7177bd3de7d7df8860405908216c18a
Synne Opheim <synnevo@gmail.com>
2015-03-06 11:17:29 +0100

    Slettet alt som stod i metoden i main.

--- src/calendar/Main.java
+++ src/calendar/Main.java
@@ -1,13 +1,11 @@
 package calendar;
 
+import Appointment.Room;
 import user.User;
+import calendar.*;
 
 public class Main {
 	public static void main(String[] args) throws Exception {
-		User user = new User("ola");
-		System.out.println(user.getPassword());
-		user.setPassword("sdf");
-		System.out.println(user.getPassword());
 		
 		//User user1 = new User("fornavn","etternavn","brukernavn","passord","mail");
 		//System.out.println(user1.getFirstname());
