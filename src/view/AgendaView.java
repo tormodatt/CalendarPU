09f9cb835e76bd4dbad1ca3b201513a90dc9219f
Synne Opheim <synnevo@gmail.com>
2015-03-11 11:39:36 +0100

    Lagt til en klasse og endret litt i view-klassene

--- src/view/AgendaView.java
+++ src/view/AgendaView.java
@@ -8,10 +8,12 @@ import Appointment.*;
 
 public class AgendaView {
 	
-	ArrayList<Appointment> appointments;
+	private User owner;
+	private ArrayList<Appointment> appointments;
 	
-	public AgendaView(ArrayList<Appointment> appointments) {
-		this.appointments = appointments;
+	public AgendaView(User owner) {
+		this.owner = owner;
+		this.appointments = owner.getPersonalCalendar(); // mangler denne metoden
 	}
 	
 	public String viewAgenda() {
