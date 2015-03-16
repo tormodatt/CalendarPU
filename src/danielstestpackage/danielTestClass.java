baaf69828c3d8b63f47124d9ff59162f71d94733
dmlundeby <daniel.m.lundeby@gmail.com>
2015-03-16 15:14:14 +0100

    Daniel: Lagt til egne klasser for testing.

---
+++ src/danielstestpackage/danielTestClass.java
@@ -0,0 +1,35 @@
+package danielstestpackage;
+
+import user.User;
+import user.Group;
+
+@SuppressWarnings("unused")
+public class danielTestClass {
+	
+	private void run() throws Exception {
+		System.out.println("Start");
+		User u1 = new User("perOlsen");
+		// Group g1 = new Group("testgruppe", u1);
+		// u1.addGroup(g1);
+		// System.out.println(u1.getGroups());
+		System.out.println("Ferdig");
+		
+//		u1.addGroup(new Group("testgruppe", u1));
+		
+		
+//		System.out.println(c.u1.getFirstname());
+		
+//		Group g = new Group("", new User("perOlsen"));
+		
+		// DEBUG START
+		// System.out.println("Group end");
+		// DEBUG END
+	}
+	
+	public static void main(String[] args) throws Exception {
+		danielTestClass c = new danielTestClass();
+		c.run();
+		
+	}
+	
+}
